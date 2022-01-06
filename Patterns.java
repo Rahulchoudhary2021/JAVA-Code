9ee735450ac4b8226f9a24f35bbcb1e0000d1cb2
Rahulchoudhary2021 <77187929+Rahulchoudhary2021@users.noreply.github.com>
2022-01-06 06:08:03 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,168 @@
+import java.util.Scanner;
+public class Patterns{
+	public static void main(String args[]){
+        // System.out.print("Enter The Number:");
+		// Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+       
+        // Pattern 1...........................
+		// for(int i=1;i<=n;i++){
+		// 	for(int j=1;j<=i;j++){
+				
+		// 	System.out.print(" * ");
+		// 	}
+		// 	System.out.println();
+		// }
+        // * 
+        // *  *
+        // *  *  *
+        // *  *  *  *
+        // *  *  *  *  * 
+        // *  *  *  *  *  *
+      
+      
+        // Pattern 2.....................
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
+        //      System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+        // *  *  *  *  *  *  *  * 
+        // *  *  *  *  *  *  *
+        // *  *  *  *  *  *
+        // *  *  *  *  * 
+        // *  *  *  *
+        // *  *  *
+        // *  *
+        // *
+
+        // Pattern 3...........................
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //     System.out.print("  ");
+        //     }
+        //     for(int j=1;j<=n-i+1;j++){
+        //      System.out.print("  *");
+        //     }
+        //     System.out.println();
+        // }
+         // *  *  *  *  *  * 
+        // *  *  *  *  *   
+        //  *  *  *  *     
+        //   *  *  *       
+        //    *  *
+        //     *
+
+        // pattern 4.....................
+//    for(int i=1;i<=n;i++){
+//        for(int j=1;j<=n-i;j++){
+//          System.out.print("  ");
+//        }
+//        for(int k=1;k<=i;k++){
+//    System.out.print("*  ");
+//        }
+//        System.out.println();
+//    }
+//             *  
+//           *  *
+//         *  *  *
+//       *  *  *  *  
+//      *  *  *  *  *
+//    *  *  *  *  *  *
+//  *  *  *  *  *  *  *
+//  * *  *  *  *  *  *  *  * 
+    
+//   Pattern 5 .....................................
+    //  int number=1;
+    //  for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //        System.out.print("  ");
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         System.out.print(number+"  ");
+    //        number++;
+    //     }
+    //     System.out.println();
+    // }
+                    //      1      
+                    //     2  3     
+                    //   4  5  6    
+                    // 7  8  9  10  
+                 // 11  12  13  14  15      
+              // 16  17  18  19  20  21    
+           // 22  23  24  25  26  27  28  
+         // 29  30  31  32  33  34  35  36  
+      // 37  38  39  40  41  42  43  44  45  
+    // 46  47  48  49  50  51  52  53  54  55
+ 
+    // Pattern 6.............................
+    
+    // int rows=2*n-1;
+    // for(int i=1;i<=rows;i++){
+    //     if(i<=n){
+    //          for(int j=1;j<=i;j++){
+    //              System.out.print("* ");
+    //          }
+    //     }else{
+    //         for(int k=1;k<=rows-i+1;k++){
+    //             System.out.print("* ");
+    //         }
+    //     }
+    //  System.out.println();
+    // }
+    // * 
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+    // * * * * * *
+    // * * * * * * *
+    // * * * * * * * *
+    // * * * * * * * * *
+    // * * * * * * * * * * 
+    // * * * * * * * * *
+    // * * * * * * * * 
+    // * * * * * * *
+    // * * * * * *
+    // * * * * *
+    // * * * *
+    // * * * 
+    // * *
+    // *
+
+    // Pattern 7........................................
+    //  System.out.println("* ");
+     // for(int i=2;i<=n-1;i++){
+     //     System.out.print("* ");
+    //      for(int j=1;j<=i-2;j++){
+    //          System.out.print("  ");
+    //      }
+    //          System.out.print("* ");
+    //          System.out.println();
+    //      }
+    //  for(int l=1;l<=n;l++){
+    //     System.out.print("* ");
+    //  }
+    //  *
+    //  * *
+    //  *   *
+    //  *     *
+    //  *       *
+    //  *         *
+    //  *           *
+    //  *             *
+    //  *               *       
+    //  *                 *     
+    //  *                   *   
+    //  *                     * 
+    //  *                       *   
+    //  * * * * * * * * * * * * * * 
+    
+ 
+	}
+	
+}
+
+
